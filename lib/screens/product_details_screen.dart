37917fb6ef4d6cffe0381c6931f71705639902ae
fahmi54321 <fahmiabdulaziz44@gmail.com>
2022-01-16 12:34:53 +0700

    working with providers pada product details screen

--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/product_providers.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
-
   static const String routeName = 'product_details';
 
   @override
   Widget build(BuildContext context) {
-
     final productId = ModalRoute.of(context)?.settings.arguments as String;
+    final loadProduct = Provider.of<ProductProvider>(
+      context,
+      listen: false, // false maksudnya tidak mentrigger update pada provider
+    ).findById(productId);
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(
+          loadProduct.title,
+        ),
       ),
     );
   }
